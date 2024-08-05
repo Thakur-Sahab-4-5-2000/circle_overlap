9a2cfc5793ed0864b73be551694e79f0caf6502e
Thakur-Sahab-4-5-2000 <shubhamhntr159753@gmail.com>
2024-08-05 16:39:55 +0530

    Circle Overlap Problem

--- src/App.js
+++ src/App.js
@@ -1,25 +1,57 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
 
-function App() {
+const App = () => {
+  const [circles, setCircles] = useState([{}]);
+
+  const getRandomColor = () => {
+    const letters = "0123456789ABCDEF";
+    let color = "#";
+    for (let i = 0; i < 6; i++) {
+      color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+  };
+
+  const handleClick = (e) => {
+    const newCircle = {
+      x: e.clientX,
+      y: e.clientY,
+      color: "blue",
+      radius: 50,
+    };
+
+    const overlapping = circles.some((circle) => {
+      const distance = Math.sqrt(
+        (circle.x - newCircle.x) ** 2 + (circle.y - newCircle.y) ** 2
+      );
+      return distance < circle.radius + newCircle.radius;
+    });
+
+    if (overlapping) {
+      newCircle.color = getRandomColor();
+    }
+
+    setCircles([...circles, newCircle]);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ width: "100vw", height: "100vh" }} onClick={handleClick}>
+      {circles.map((circle, index) => (
+        <div
+          key={index}
+          style={{
+            width: circle.radius * 2,
+            height: circle.radius * 2,
+            borderRadius: "50%",
+            backgroundColor: circle.color,
+            position: "absolute",
+            left: circle.x - circle.radius,
+            top: circle.y - circle.radius,
+          }}
+        ></div>
+      ))}
     </div>
   );
-}
+};
 
 export default App;
